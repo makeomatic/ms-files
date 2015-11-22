736ac4e998ccbcee23263fd4155148fc39eb7f30
AVVS <v@aminev.me>
2015-11-23 01:23:16 +0300

    More options

--- src/index.js
+++ src/index.js
@@ -29,14 +29,30 @@ module.exports = class Files extends Mservice {
     prefix: 'files',
     // postfixes for routes
     postfix: {
-      // constructs signed download URL for the file
+      // constructs signed download URL for the file. if an <id> is provided - checks access rights
+      // if not - just gives a URL.
       download: 'download',
       // constructs resumable upload
+      // binds it to provided <id>. If upload is not finished in X hours
+      // URL is revoked, any information regarding this id is erased as well
       upload: 'upload',
-      // sends uploaded file for post processing
+      // notify about finished file upload
+      // use this to store data associated with upload <id> permanently
+      // and launch post process sequence
+      finish: 'finish',
+      // sends file for post processing
+      // can be used from `finish` route, as well as on demand
       process: 'process',
       // provides information for the requested file
+      // if <id> is specified, access rights are checked before returning the info
       info: 'info',
+      // list - provides list of files that belong to some <id>, allows sorting, filtering and paginating
+      // if no id is specified - iterates over all files that were uploaded
+      list: 'list',
+    },
+    // amqp options
+    amqp: {
+      queue: 'ms-files',
     },
     // storage options
     redis: {
@@ -45,7 +61,12 @@ module.exports = class Files extends Mservice {
       },
     },
     // default storage for files
-    transport: 'gce',
+    transport: {
+      // transport name
+      name: 'gce',
+      // provide config options
+      options: {},
+    },
     // function that is used in post-processing of uploaded files
     process: function noop() {
       return Promise.resolve();
@@ -64,8 +85,8 @@ module.exports = class Files extends Mservice {
     });
 
     // init file transfer provider
-    const Provider = require(`ms-files-${config.transport}`);
-    this.provider = new Provider(config[config.transport]);
+    const Provider = require(`ms-files-${config.transport.name}`);
+    this.provider = new Provider(config.transport.options);
   }
 
   /**
