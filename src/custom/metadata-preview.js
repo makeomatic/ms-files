66583edadbfd25a6d8d785389029928e927b7267
AVVS <v@aminev.me>
2015-12-24 00:36:00 +0300

    fix(custom/metadata-preview) preview length adjustment

--- src/custom/metadata-preview.js
+++ src/custom/metadata-preview.js
@@ -10,7 +10,7 @@ module.exports = function extractMetadata(provider, { key, data, redis }) {
     provider.readFile(filename, HASH_SIZE),
   )
   .spread(function addMetadata(image, md5) {
-    const imageLength = parseInt(image.contents, 10);
+    const imageLength = image.contents.readInt32BE(0);
     const md5Hash = md5.contents.toString('hex');
     const contentLength = parseInt(image.response.headers['content-length'], 10) - 20 - imageLength;
 
