b057ab11feb87126c287c1234941bcb13497a2ef
AVVS <v@aminev.me>
2015-12-25 00:05:23 +0300

    Fix custom preview script

--- src/custom/metadata-preview.js
+++ src/custom/metadata-preview.js
@@ -1,22 +1,18 @@
 const Promise = require('bluebird');
 const IMAGE_SIZE = { start: 0, end: 3 };
-const HASH_SIZE = { end: -16 };
+const HASH_SIZE = 16;
 
 module.exports = function extractMetadata(provider, { key, data, redis }) {
   const { filename } = data;
 
   return Promise.join(
     provider.readFile(filename, IMAGE_SIZE),
-    provider.readFile(filename, HASH_SIZE),
+    provider.readFile(filename, { start: data.contentLength - HASH_SIZE }),
   )
   .spread(function addMetadata(image, md5) {
-    console.log(image);
-    console.log(md5);
-    console.log(data.contentLength);
-
     const imageLength = image.contents.readInt32BE(0);
     const md5Hash = md5.contents.toString('hex');
-    const contentLength = parseInt(image.response.headers['content-length'], 10) - 20 - imageLength;
+    const contentLength = parseInt(image.response.headers['x-goog-stored-content-length'], 10) - 20 - imageLength;
     const output = {
       previewSize: imageLength,
       modelSize: contentLength,
