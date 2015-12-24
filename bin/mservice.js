a0154d442f824d5a97abddc14fdf7c9d01c4ed62
AVVS <v@aminev.me>
2015-12-24 20:43:06 +0300

    require configuration after NODE_ENV

--- bin/mservice.js
+++ bin/mservice.js
@@ -2,10 +2,6 @@
 
 'use strict';
 
-// accepts conf through .env file
-// suitable for configuring this in the docker env
-var configuration = require('ms-amqp-conf');
-
 var dir;
 if (process.env.NODE_ENV === 'production') {
   dir = '../lib';
@@ -14,6 +10,9 @@ if (process.env.NODE_ENV === 'production') {
   require('babel-register');
 }
 
+// accepts conf through .env file
+// suitable for configuring this in the docker env
+var configuration = require('ms-amqp-conf');
 var Service = require(dir);
 var service = new Service(configuration);
 service.connect()
--- src/custom/metadata-preview.js
+++ src/custom/metadata-preview.js
@@ -10,6 +10,10 @@ module.exports = function extractMetadata(provider, { key, data, redis }) {
     provider.readFile(filename, HASH_SIZE),
   )
   .spread(function addMetadata(image, md5) {
+    console.log(image);
+    console.log(md5);
+    console.log(data.contentLength);
+
     const imageLength = image.contents.readInt32BE(0);
     const md5Hash = md5.contents.toString('hex');
     const contentLength = parseInt(image.response.headers['content-length'], 10) - 20 - imageLength;
