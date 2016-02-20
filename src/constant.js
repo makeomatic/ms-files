0a060259637faff5ff2d0b372084e41fca5e9693
Vitaly Aminev <AVVS@users.noreply.github.com>
2016-02-20 19:10:27 +0300

    fix: files-data to files-index

--- src/constant.js
+++ src/constant.js
@@ -2,7 +2,7 @@ module.exports = {
   STATUS_PENDING: '1',
   STATUS_UPLOADED: '2',
   STATUS_PROCESSED: '3',
-  FILES_INDEX: 'files-data',
+  FILES_INDEX: 'files-index',
   FILES_DATA: 'files-data',
   UPLOAD_DATA: 'upload-data',
   PREVIEW_PREFIX: 'preview:',
