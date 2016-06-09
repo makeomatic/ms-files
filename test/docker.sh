a0fe83ef9b5853732f08080cd1f9a860f79b41f3
AVVS <v@aminev.me>
2016-06-10 00:12:42 +0300

    tests: update native modules preparation

--- test/docker.sh
+++ test/docker.sh
@@ -29,9 +29,17 @@ trap finish EXIT
 export IMAGE=makeomatic/node:$NODE_VER
 $COMPOSE -f $DC up -d
 
+# add glibc
+$COMPOSE -f $DC exec tester /bin/sh -c "apk --no-cache add ca-certificates openssl make g++ python linux-headers \
+  && printf '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApZ2u1KJKUu/fW4A25y9m\ny70AGEa/J3Wi5ibNVGNn1gT1r0VfgeWd0pUybS4UmcHdiNzxJPgoWQhV2SSW1JYu\ntOqKZF5QSN6X937PTUpNBjUvLtTQ1ve1fp39uf/lEXPpFpOPL88LKnDBgbh7wkCp\nm2KzLVGChf83MS0ShL6G9EQIAUxLm99VpgRjwqTQ/KfzGtpke1wqws4au0Ab4qPY\nKXvMLSPLUp7cfulWvhmZSegr5AdhNw5KNizPqCJT8ZrGvgHypXyiFvvAH5YRtSsc\nZvo9GI2e2MaZyo9/lvb+LbLEJZKEQckqRj4P26gmASrZEPStwc+yqy1ShHLA0j6m\n1QIDAQAB\n-----END PUBLIC KEY-----\n' > /etc/apk/keys/sgerrand.rsa.pub \
+  && wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.23-r2/glibc-2.23-r2.apk \
+  && apk add glibc-2.23-r2.apk \
+  && rm glibc-2.23-r2.apk" || exit 1
+
 if [[ "$SKIP_REBUILD" != "1" ]]; then
   echo "rebuilding native dependencies..."
-  $COMPOSE -f $DC exec tester npm rebuild
+  $COMPOSE -f $DC exec tester npm rebuild hiredis
+  $COMPOSE -f $DC exec tester npm rebuild grpc --update-binary
 fi
 
 echo "cleaning old coverage"
