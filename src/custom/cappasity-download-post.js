38c8485ba2d96a54a3a3080a3e9adb1172d04441
AVVS <v@aminev.me>
2016-07-31 20:41:46 +0300

    fix: correctly return remapped URLs for cappasity

--- src/custom/cappasity-download-post.js
+++ src/custom/cappasity-download-post.js
@@ -5,7 +5,8 @@ const PROPS_TO_ADD = ['model', 'obj', 'wrl', 'stl'];
  * Decorates output with extra data to ease fetching of links
  */
 module.exports = function decorateOutput(originalData, output) {
-  const files = originalData.files;
+  const files = output.files;
+
   PROPS_TO_ADD.forEach(prop => {
     const nonResolvedLink = originalData[prop];
     if (nonResolvedLink) {
