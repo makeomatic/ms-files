918c31d5bc74ad378e9e22f07a376d8d2363b6d3
Vitaly Aminev <AVVS@users.noreply.github.com>
2016-08-29 20:09:39 +0300

    feat: determine extension based on type or contentType (#32)
    feat: determine extension based on type or contentType
    fix: correctly use router.routes.prefix when determening route

---
+++ src/utils/extension.js
@@ -0,0 +1,15 @@
+const { TYPE_MAP } = require('../constant.js');
+const { extension } = require('mime-types');
+
+/**
+ * Returns extension based on predefined type or mime-type
+ * @param  {String} type
+ * @param  {String} contentType
+ * @return {String}
+ */
+const typeToExtension = (type, contentType) =>
+  TYPE_MAP[type] ||
+  extension(contentType) ||
+  '.bin';
+
+module.exports = typeToExtension;
