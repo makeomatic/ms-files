57800258ffc68d2cf24c7d51fa089bc58723c9d0
Evgeny Poyarkov <eug.poyarkov@gmail.com>
2016-10-12 19:29:01 +0300

    fix: change format of backgroundImage field

--- src/utils/isValidBackgroundOrigin.js
+++ src/utils/isValidBackgroundOrigin.js
@@ -5,15 +5,13 @@ module.exports = function isValidBackgroundOrigin(data) {
   const backgroundImage = data[FILES_BACKGROUND_IMAGE_FIELD];
 
   if (backgroundImage) {
-    const { url } = backgroundImage;
-
     /**
      *  Using 'upload' selector here since background images are always
      *  contained in the persistent store
      *  https://github.com/makeomatic/ms-files/blob/master/src/custom/cappasity-select-bucket.js#L10
      */
     const provider = this.provider('upload', data);
-    const isValidOrigin = url.indexOf(provider.cname) === 0;
+    const isValidOrigin = backgroundImage.match(`^${provider.cname}(.*)\.(png|jpeg|jpg)$`);
 
     if (!isValidOrigin) {
       throw new HttpStatusError(412, 'invalid origin');
