22b933b3075d062813a3a9f3a374337c6620eb2f
Vitaly Aminev <AVVS@users.noreply.github.com>
2016-11-21 22:47:01 +0300

    fix: dynamic default based on upload type
    * fix/hidefullscreen-for-image-model
    
    * fix
    
    * fixes

--- src/custom/cappasity-info-post.js
+++ src/custom/cappasity-info-post.js
@@ -5,38 +5,46 @@ const {
   FILES_PLAYER_HIDECONTROLS,
   FILES_PLAYER_HIDEFULLSCREEN,
   FILES_PLAYER_LOGO,
+
+  CAPPASITY_IMAGE_MODEL,
 } = require('../constant.js');
 
-const defaultPlayerOpts = {
-  [FILES_PLAYER_AUTORUN]: {
-    type: 'boolean',
-    default: 0,
-    description: 'Auto-start player',
-  },
-  [FILES_PLAYER_CLOSEBUTTON]: {
-    type: 'boolean',
-    default: 1,
-    description: 'Show close button',
-    paid: true,
-  },
-  [FILES_PLAYER_HIDECONTROLS]: {
-    type: 'boolean',
-    default: 0,
-    description: 'Hide player controls',
-    paid: true,
-  },
-  [FILES_PLAYER_LOGO]: {
-    type: 'boolean',
-    default: 1,
-    description: 'Show logo',
-    paid: true,
-  },
-  [FILES_PLAYER_HIDEFULLSCREEN]: {
-    type: 'boolean',
-    default: 0,
-    description: 'Hide fullscreen button',
-  },
-};
+function getPlayerOpts({ uploadType }) {
+  const isImageModel = uploadType === CAPPASITY_IMAGE_MODEL;
+  const defaultPlayerOpts = {
+    [FILES_PLAYER_AUTORUN]: {
+      type: 'boolean',
+      default: 0,
+      description: 'Auto-start player',
+    },
+    [FILES_PLAYER_CLOSEBUTTON]: {
+      type: 'boolean',
+      default: 1,
+      description: 'Show close button',
+      paid: true,
+    },
+    [FILES_PLAYER_HIDECONTROLS]: {
+      type: 'boolean',
+      description: 'Hide player controls',
+      default: 0,
+      paid: true,
+      disabled: isImageModel,
+    },
+    [FILES_PLAYER_LOGO]: {
+      type: 'boolean',
+      default: 1,
+      description: 'Show logo',
+      paid: true,
+    },
+    [FILES_PLAYER_HIDEFULLSCREEN]: {
+      type: 'boolean',
+      default: isImageModel ? 1 : 0,
+      description: 'Hide fullscreen button',
+    },
+  };
+
+  return defaultPlayerOpts;
+}
 
 const defaultWindowOptions = {
   width: {
@@ -73,10 +81,11 @@ module.exports = function getEmbeddedInfo(file) {
   const { uploadId: id, status } = file;
 
   if (status === STATUS_PROCESSED) {
+    const options = getPlayerOpts(file);
     file.embed = {
-      code: getEmbeddedCode(id, getQueryString(defaultPlayerOpts)),
+      code: getEmbeddedCode(id, getQueryString(options)),
       params: {
-        ...defaultPlayerOpts,
+        ...options,
         ...defaultWindowOptions,
       },
     };
