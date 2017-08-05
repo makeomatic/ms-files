1ad8e74a9828b6d8675b71fde84a43e53f61805e
AVVS <v@makeomatic.ru>
2017-08-05 11:56:37 -0700

    feat(onComplete): upgraded @microfleet/core & default settings

--- src/configs/amqp.js
+++ src/configs/amqp.js
@@ -1,4 +1,6 @@
-const resolveMessage = require('../messageResolver');
+// make sure we dont reject .process right away
+const processRegExp = /\.process$/;
+const isProcess = name => processRegExp.test(name);
 
 exports.amqp = {
   transport: {
@@ -6,10 +8,27 @@ exports.amqp = {
     queue: 'ms-files',
     // we need QoS for certain operations
     neck: 100,
-    // add default onComplete handelr
-    onComplete: resolveMessage,
+    // bind headers
+    bindPersistantQueueToHeadersExchange: true,
   },
   router: {
     enabled: true,
+  },
+  retry: {
+    enabled: true,
+    min: 100,
+    max: 3000,
+    factor: 2,
+    maxRetries: 5,
+    predicate(error, actionName) {
+      switch (error.name) {
+        case 'ValidationError':
+        case 'HttpStatusError':
+          return true;
+
+        default:
+          return isProcess(actionName) === false;
+      }
+    },
   },
 };
