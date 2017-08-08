5e02840d9092496eb949457244bcac43da44c41d
AVVS <v@makeomatic.ru>
2017-08-08 14:38:26 -0700

    feat(simple-upload): rework to use only this service
    This is useful for implementing e2e tests in other services.
    On top of it modify cappasity hook

--- src/custom/cappasity-info-post.js
+++ src/custom/cappasity-info-post.js
@@ -5,7 +5,6 @@ const {
 
   FILES_PLAYER_AUTORUN,
   FILES_PLAYER_CLOSEBUTTON,
-  FILES_PLAYER_HIDECONTROLS,
   FILES_PLAYER_HIDEFULLSCREEN,
   FILES_PLAYER_LOGO,
 
@@ -19,47 +18,118 @@ const {
              0 : free, 10 : lite, 20: basic, 30 : pro
  */
 
-function getPlayerOpts({ uploadType }) {
-  const isImageModel = uploadType === CAPPASITY_IMAGE_MODEL;
-  const defaultPlayerOpts = {
-    [FILES_PLAYER_AUTORUN]: {
-      type: 'boolean',
-      default: 0,
-      description: 'Auto-start player',
-    },
-    [FILES_PLAYER_CLOSEBUTTON]: {
-      type: 'boolean',
-      default: 1,
-      description: 'Show close button',
-      paid: true,
-      reqPlanLevel: 30,
-    },
-    [FILES_PLAYER_HIDECONTROLS]: {
-      type: 'boolean',
-      description: 'Hide player controls',
-      default: 0,
-      paid: true,
-      reqPlanLevel: 30,
-    },
-    [FILES_PLAYER_LOGO]: {
-      type: 'boolean',
-      default: 1,
-      description: 'Show logo',
-      paid: true,
-      reqPlanLevel: 20,
-    },
-    [FILES_PLAYER_HIDEFULLSCREEN]: {
-      type: 'boolean',
-      default: isImageModel ? 1 : 0,
-      description: 'Hide fullscreen button',
-      disabled: isImageModel,
-    },
-  };
+// adds key={{ key }} to .hbs-like template
+const prepareTemplate = key => `${key}={{ ${key} }}`;
+const getQueryString = params => (
+  Object.keys(params).map(prepareTemplate).join('&')
+);
 
-  return defaultPlayerOpts;
-}
+// default options
+const defaultPlayerOpts = Object.setPrototypeOf({
+  [FILES_PLAYER_AUTORUN]: {
+    type: 'boolean',
+    default: 0,
+    description: 'Auto-start player',
+    section: 'generic',
+  },
+  [FILES_PLAYER_CLOSEBUTTON]: {
+    type: 'boolean',
+    default: 1,
+    description: 'Show close button',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  [FILES_PLAYER_LOGO]: {
+    type: 'boolean',
+    default: 1,
+    description: 'Show logo',
+    paid: true,
+    reqPlanLevel: 20,
+    section: 'generic',
+  },
+  enableimagezoom: {
+    type: 'boolean',
+    default: 1,
+    description: 'Enable Image Zoom',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  zoomquality: {
+    type: 'integer',
+    default: 1,
+    enum: {
+      SD: 1,
+      HD: 2,
+    },
+    description: 'Force Quality Settings',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  autorotate: {
+    type: 'boolean',
+    default: 0,
+    description: 'Autorotate 3D View',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  autorotatetime: {
+    type: 'float',
+    default: 10,
+    min: 0.1,
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  autorotatedelay: {
+    type: 'float',
+    default: 2,
+    min: 0.1,
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  autorotatedir: {
+    type: 'integer',
+    default: 1,
+    enum: {
+      clockwise: 1,
+      'counter-clockwise': -1,
+    },
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  hidezoomopt: {
+    type: 'boolean',
+    default: 0,
+    description: 'Hide Zoom Option',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  hideautorotateopt: {
+    type: 'boolean',
+    default: 0,
+    description: 'Hide Autorotate Option',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+  hidesettingsbtn: {
+    type: 'boolean',
+    default: 0,
+    description: 'Hide Settings Button',
+    paid: true,
+    reqPlanLevel: 30,
+    section: 'pro',
+  },
+}, null);
 
-const defaultWindowOptions = {
+const defaultWindowOptions = Object.setPrototypeOf({
   width: {
     type: 'string',
     default: '100%',
@@ -70,42 +140,58 @@ const defaultWindowOptions = {
     default: '600px',
     description: 'Height of embedded window (px or %)',
   },
+}, null);
+
+const defaultQS = getQueryString(defaultPlayerOpts);
+
+const defaultIframe = `<iframe
+  allowfullscreen
+  mozallowfullscreen="true"
+  webkitallowfullscreen="true"
+  width="{{ width }}"
+  height="{{ height }}"
+  frameborder="0"
+  style="border:0;"
+  onmousewheel=""
+  src="https://api.cappasity.com/api/player/{{ id }}/embedded?${defaultQS}&{{ extraQS }}"
+></iframe>`.replace(/\s+/g, ' ');
+
+const getPlayerOpts = ({ uploadType }) => {
+  const isImageModel = uploadType === CAPPASITY_IMAGE_MODEL;
+  return {
+    [FILES_PLAYER_HIDEFULLSCREEN]: {
+      type: 'boolean',
+      description: 'Hide fullscreen button',
+      default: isImageModel ? 1 : 0,
+      disabled: isImageModel,
+    },
+  };
 };
 
-function getQueryString(params) {
-  return Object.keys(params).map(key => `${key}={{ ${key} }}`).join('&');
-}
-
-function getEmbeddedCode(id, qs) {
-  return `<iframe
-    allowfullscreen
-    mozallowfullscreen="true"
-    webkitallowfullscreen="true"
-    width="{{ width }}"
-    height="{{ height }}"
-    frameborder="0"
-    style="border:0;"
-    onmousewheel=""
-    src="https://api.cappasity.com/api/player/${id}/embedded?${qs}"
-  ></iframe>`.replace(/\s+/g, ' ');
-}
+const getEmbeddedCode = (id, qs) => (
+  defaultIframe
+    .replace('{{ id }}', id)
+    .replace('{{ extraQS }}', qs)
+);
 
 // cached allowed statuses
-const GREEN_LIGHT_STATUSES = {
+const GREEN_LIGHT_STATUSES = Object.setPrototypeOf({
   [STATUS_PROCESSED]: true,
   [STATUS_PROCESSING]: true,
   [STATUS_FAILED]: true,
-};
+}, null);
 
 module.exports = function getEmbeddedInfo(file) {
   const { uploadId: id, status } = file;
 
   if (GREEN_LIGHT_STATUSES[status] === true) {
-    const options = getPlayerOpts(file);
+    const dynamicOptions = getPlayerOpts(file);
+
     file.embed = {
-      code: getEmbeddedCode(id, getQueryString(options)),
+      code: getEmbeddedCode(id, getQueryString(dynamicOptions)),
       params: {
-        ...options,
+        ...defaultPlayerOpts,
+        ...dynamicOptions,
         ...defaultWindowOptions,
       },
     };
---
+++ test/suites/bin/simple-upload.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+
+const assert = require('assert');
+const path = require('path');
+
+// helpers
+const {
+  startService,
+  stopService,
+  owner,
+  bindSend,
+} = require('../../helpers/utils');
+
+describe('binary: simple-upload', function suite() {
+  const binaryPath = path.resolve(__dirname, '../../../bin/simple-upload.js');
+  const exec = require('../../helpers/exec')(binaryPath, ['-u', owner]);
+
+  before('start service', startService);
+  before('helpers', bindSend('files.info'));
+  after('stop service', stopService);
+
+  const opts = [
+    '-f', './test/fixtures/sample-pack',
+    '-p', './test/fixtures/sample-pack/preview.jpeg',
+    '-n', 'pack sample',
+    '--public',
+    '--exclude-preview',
+    '--report-finish',
+  ];
+
+  it('dry run: prepares upload message', function test() {
+    return exec([...opts]).then((lines) => {
+      assert.deepEqual(lines, [
+        'resolved 1 file(s)',
+        'Dry run, printing prepared message:',
+        '',
+        '',
+        '{ username: \'v@makeomatic.ru\',',
+        '  meta: { name: \'pack sample\' },',
+        '  access: { setPublic: true },',
+        '  uploadType: \'simple\',',
+        '  resumable: false,',
+        '  temp: false,',
+        '  unlisted: false,',
+        '  files: ',
+        '   [ { type: \'c-preview\',',
+        '       contentType: \'image/jpeg\',',
+        '       contentLength: 64024,',
+        '       md5Hash: \'03506d1122d213baa1445f6b5655d2e4\' },',
+        '     { type: \'c-pack\',',
+        '       contentType: \'image/vnd.cappasity\',',
+        '       contentLength: 21680,',
+        '       md5Hash: \'4f91e44f50cc8647611606eff0ada50e\' } ] }',
+      ]);
+
+      return null;
+    });
+  });
+
+  it('confirm: sends upload', function test() {
+    return exec([...opts, '--confirm'])
+      .then((lines) => {
+        const uploadId = lines[3];
+        assert.ok(uploadId);
+        return this.send({ username: owner, filename: uploadId });
+      })
+      .then((response) => {
+        assert.equal(response.status, '3');
+        return null;
+      });
+  });
+});
