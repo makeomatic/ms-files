2783cf44cca530d2fb4100be78db3d695c3ab426
AVVS <v@makeomatic.ru>
2017-09-29 12:59:17 -0700

    patch: node 8.6.0, leak when lock fails to acquire
    NOTE: apply-sku has also been update to allow overwriting alias based
    on startedAt timestamp

--- bin/apply-sku.js
+++ bin/apply-sku.js
@@ -29,12 +29,15 @@ const iterator = {
   offset: 0,
   limit: 20,
   owner: argv.user,
+  criteria: 'startedAt',
+  order: 'ASC',
   filter: {
     alias: {
       isempty: true,
     },
   },
   without: ['files'],
+  expiration: 100, // small cache size
 };
 
 // App level code
@@ -54,12 +57,19 @@ const getInfo = (amqp, filename) => (
 const isNewer = (amqp, newUploadId, existingUploadId) => (
   Promise
     .props({
-      newData: getInfo(newUploadId).get('file').get('startedAt'),
-      data: getInfo(existingUploadId).get('file').get('startedAt'),
+      newData: getInfo(amqp, newUploadId).get('file').get('startedAt'),
+      data: getInfo(amqp, existingUploadId).get('file').get('startedAt'),
     })
-    .then(({ newData, data }) => (
-      newData - data > 0
-    ))
+    .then(({ newData, data }) => {
+      const isNew = newData - data > 0;
+
+      if (isNew === false) {
+        console.info('[warn] overwrite failed: %s older than %s - [%s / %s] by %s',
+          newUploadId, existingUploadId, newData, data, newData - data);
+      }
+
+      return isNew;
+    })
 );
 
 // perform update
@@ -67,7 +77,7 @@ const performUpdate = (amqp, file) => {
   const { name, uploadId } = file;
 
   if (filter && filter.test(name) !== true) {
-    console.warn('[skip] %s -> %s because of %s', uploadId, name, argv.filter);
+    console.info('[skip] %s -> %s because of %s', uploadId, name, argv.filter);
     return null;
   }
 
@@ -85,8 +95,8 @@ const performUpdate = (amqp, file) => {
     .catch({ statusCode: 409 }, (e) => {
       if (argv.overwrite !== true) throw e;
 
-      console.warn('[warn] checking for overwrite %s for %s on %s', name, uploadId, e.data.uploadId);
-      return isNewer(uploadId, e.data.uploadId).then((yes) => {
+      console.info('[warn] checking for overwrite of sku <%s>. Currently on <%s>, wants to be on <%s>', name, e.data.uploadId, uploadId);
+      return isNewer(amqp, uploadId, e.data.uploadId).then((yes) => {
         // it's not newer, sorry
         if (yes === false) throw e;
 
@@ -95,7 +105,7 @@ const performUpdate = (amqp, file) => {
       });
     })
     .catch((e) => {
-      console.warn('[warn] failed to set alias to %s for %s: %s', name, uploadId, e.message);
+      console.info('[warn] failed to set alias <%s> for <%s>: %s', name, uploadId, e.message);
     });
 };
 
@@ -110,7 +120,7 @@ const listAndUpdate = (amqp) => {
         console.info('pages found: %d', data.pages);
       }
     })
-    .tap(data => Promise.map(data.files, file => performUpdate(amqp, file)))
+    .tap(data => Promise.map(data.files, file => performUpdate(amqp, file), { concurrency: 1 }))
     .tap((data) => {
       // recursively work on the next page
       if (data.page < data.pages) {
