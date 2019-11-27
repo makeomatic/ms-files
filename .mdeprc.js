5e1f2b418b6d517fc57ec8601f09c3cc5c7c5d5c
Eugene Poyarkov <eug.poyarkov@gmail.com>
2019-11-27 12:02:07 +0300

    fix: change sleep time

--- .mdeprc.js
+++ .mdeprc.js
@@ -2,7 +2,7 @@ module.exports = {
   "node": "12.13.0",
   "auto_compose": true,
   "with_local_compose": true,
-  "sleep": 15,
+  "sleep": 35,
   "services": [
     "rabbitmq",
     "redisSentinel",
