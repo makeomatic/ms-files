d9015eb1afd341667a1b7d2a888994433235ed97
Vitaly Aminev <AVVS@users.noreply.github.com>
2020-02-25 13:00:35 -0800

    feat: additional 3d ar props (#183)
    * feat: ar-3d-view props, additional upload types
    * fix: cyrillic c to proper c
    * fix: typos
    * chore: cleanup coverage on local machines

--- .mdeprc.js
+++ .mdeprc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  "node": "12.14.1",
-  "auto_compose": true,
-  "with_local_compose": true,
-  "sleep": 35,
-  "services": [
-    "rabbitmq",
-    "redisSentinel",
-    "redisCluster"
-  ],
-  "nycReport": false,
-  "arbitrary_exec": "yarn coverage:clean",
-  "post_exec": "yarn coverage:report",
+  node: "12.15.0",
+  auto_compose: true,
+  with_local_compose: true,
+  sleep: 35,
+  services: ['rabbitmq'],
+  nycReport: false,
+  post_exec: 'yarn coverage:report'
+};
+
+switch (process.env.DB) {
+  case 'sentinel':
+    module.exports.services.push('redisSentinel');
+    break;
+  case 'cluster':
+    module.exports.services.push('redisCluster')
+    break;
+}
+
+if (process.env.CI !== 'true') {
+  module.exports.arbitrary_exec = 'yarn coverage:clean';
 }
