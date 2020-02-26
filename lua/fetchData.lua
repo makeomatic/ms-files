8808bef564d6d7fb08e3ad2c2db79a0ee8777355
pajgo <pavel.rogovoi@gmail.com>
2020-02-26 10:31:50 +0600

    fix: no lua keys if pick passed

--- lua/fetchData.lua
+++ lua/fetchData.lua
@@ -1,11 +1,11 @@
 -- first key is redis hash key
 -- we need to determine if it exists or not and if it does - return error
 local key = KEYS[1]
-local hashKeys = redis.call('hkeys', key)
+local redisCall = redis.call
 local next = next
 
--- no keys, doesnt exist
-if next(hashKeys) == nil then
+-- key type ~= hash means not exists
+if redisCall('type', key).ok ~= 'hash' then
   return redis.error_reply('404');
 end
 
@@ -18,30 +18,25 @@ local params
 if type(ARGV[1]) == 'string' then params=cjson.decode(ARGV[1]) end
 
 local omit = (type(params['omit']) == 'table' and params['omit'] or {})
-local pick = (type(params['pick']) == 'table' and params['pick'] or hashKeys)
+local pick = (type(params['pick']) == 'table' and params['pick'] or redisCall('hkeys', key))
 
 local filtered
 
 if next(params) ~= nil then
   local insert = table.insert
   local omitTree = {}
-  local pickTree = {}
 
   for _,field in pairs(omit) do
     omitTree[field] = true
   end
 
-  for _,field in pairs(pick) do
-    pickTree[field] = true
-  end
-
   filtered = {}
-  for _,field in pairs(hashKeys) do
+  for _,field in pairs(pick) do
     -- if field present in omit we exclude it from pick list
-    if (omitTree[field] ~= true) and (pickTree[field] == true) then insert(filtered, field) end
+    if (omitTree[field] ~= true) then insert(filtered, field) end
   end
 else
-  filtered = hashKeys
+  filtered = pick
 end
 
 -- no fields left
@@ -50,7 +45,7 @@ if #filtered == 0 then
 end
 
 -- fetched data
-local data = redis.call('hmget', key, unpack(filtered))
+local data = redisCall('hmget', key, unpack(filtered))
 
 -- return 2 lists of keys & associated data
 return {filtered, data};
