5d3e5a3fea9b74c05c44f4848e464a2f8479a85e
Sasha Sorokin <sorokin.sh@gmail.com>
2020-12-05 02:53:34 +0300

    fix: tests

--- test/suites/providers/oss.js
+++ test/suites/providers/oss.js
@@ -5,9 +5,8 @@ describe('util fetch-data suite', () => {
   it('should be able to create instance', () => {
     const provider = new OSSTransport({
       options: {
-        // from env
-        // accessKeyId: '',
-        // accessKeySecret: '',
+        accessKeyId: 'Perchik',
+        accessKeySecret: 'FaTCaT',
         bucket: 'perchik',
         region: 'cn',
         secure: true,
@@ -16,7 +15,7 @@ describe('util fetch-data suite', () => {
 
     strictEqual(
       // eslint-disable-next-line max-len
-      /https:\/\/perchik\.cn\.aliyuncs\.com\/4fb4611d312488add2ca6b7fb62ddec2\/a97de2c6-349e-4f0f-ac85-fd6f1de581b7\/ae77baf5-bb24-4032-bbe8-9f59594dfb33\.pack\?OSSAccessKeyId=LTAI4G3Z3EK7G5K7Uy7gohVs&Expires=(\d+)&Signature=([\w\d\-%]+)&response-content-disposition=5100015755485/
+      /https:\/\/perchik\.cn\.aliyuncs\.com\/4fb4611d312488add2ca6b7fb62ddec2\/a97de2c6-349e-4f0f-ac85-fd6f1de581b7\/ae77baf5-bb24-4032-bbe8-9f59594dfb33\.pack\?OSSAccessKeyId=Perchik&Expires=(\d+)&Signature=([\w\d\-%]+)&response-content-disposition=5100015755485/
         .test(provider.getDownloadUrlSigned(
           '4fb4611d312488add2ca6b7fb62ddec2/a97de2c6-349e-4f0f-ac85-fd6f1de581b7/ae77baf5-bb24-4032-bbe8-9f59594dfb33.pack',
           '5100015755485'
