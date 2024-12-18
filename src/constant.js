5dbc6b68a76a80ad78e4cbad6d9461fa8cafdae9
annahassel <sorokin.sh@gmail.com>
2024-12-18 22:20:44 +0300

    Revert "feat: additional content (#339)" (#340)
    This reverts commit 46feddda790b848b74a66bf3a763d9bc8e8268db.

--- src/constant.js
+++ src/constant.js
@@ -72,7 +72,6 @@ module.exports = exports = Object.setPrototypeOf({
   FILES_NFT_BLOCK_FIELD: 'nftBlock',
   FILES_HAS_REFERENCES_FIELD: 'hasReferences',
   FILES_REFERENCES_FIELD: 'references',
-  FILES_ADDITIONAL_CONTENT_FIELD: 'additionalContent',
   FILES_IS_REFERENCED_FIELD: 'isReferenced',
   FILES_IS_IN_SHOWROOM_FIELD: 'isInShowroom',
   FILES_CATEGORIES_FIELD: 'categories',
@@ -120,7 +119,6 @@ exports.FIELDS_TO_STRINGIFY = [
   exports.FILES_PLAYER_FRAMES_CYCLE_FIELD,
   exports.FILES_NFT_FIELD,
   exports.FILES_REFERENCES_FIELD,
-  exports.FILES_ADDITIONAL_CONTENT_FIELD,
 ];
 
 exports.FIELDS_TO_UNTAG = {
